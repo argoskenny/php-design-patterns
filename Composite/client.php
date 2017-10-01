ef938e9353f75c7687a9ce4486b2eb3a0f491b91
argoskenny <argoskenny@gmail.com>
2017-10-01 23:05:45 +0800

    Composite pattern client example add more complex level

--- Composite/client.php
+++ Composite/client.php
@@ -1,11 +1,22 @@
 <?php
 include 'MenuComposite.php';
 
+$cake = new Item("蛋糕","70");
+$pudding = new Item("布丁","50");
+$dessert = new Menu(array($cake, $pudding));
+
 $coffee = new Item("咖啡","80");
 $juice = new Item("果汁","70");
 $milktea = new Item("奶茶","50");
 
-$drinks = array($coffee, $juice, $milktea);
-$menu = new Menu($drinks);
+$sideDish = new Menu(array($dessert, $coffee, $juice, $milktea));
+
+
+$cheeseBurger = new Item("起士漢堡","230");
+$fishburger = new Item("鱈魚堡","210");
+
+$mainCourst = new Menu(array($cheeseBurger, $fishburger));
+
+$menu = new Menu(array($sideDish, $mainCourst));
 
 $menu->getNameAndPrice();
