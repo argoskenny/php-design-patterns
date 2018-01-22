61449a6e62b942be3da15546f43ec5b5fc47a3e1
Kenny Lin <KennyLin@beautifulcard.com>
2018-01-22 17:57:55 +0800

    add Composite search feature

--- Composite/MenuComposite.php
+++ Composite/MenuComposite.php
@@ -2,6 +2,7 @@
 abstract class MenuComposite
 {
     abstract function getNameAndPrice();
+    abstract function getItemByName($itemName);
 }
 
 class Item extends MenuComposite
@@ -17,20 +18,41 @@ class Item extends MenuComposite
     public function getNameAndPrice() {
         echo $this->name." - ".$this->price."<br>";
     }
+
+    public function getItemByName($itemName) {
+        if ($this->name == $itemName) {
+            return $this->name." - ".$this->price."<br>";
+        }
+        return NULL;
+    }
 }
 
 class Menu extends MenuComposite
 {
     private $items = [];
+    private $menuName;
 
-    public function __construct($items) {
+    public function __construct($items, $menuName) {
         $this->items = $items;
+        $this->menuName = $menuName;
     }
 
     public function getNameAndPrice() {
+        echo "MENU: ".$this->menuName."<br>";
         foreach ($this->items as $item) {
             $item->getNameAndPrice();
         }
+    }
+
+    public function getItemByName($itemName) {
+        $result = NULL;
+        foreach ($this->items as $item) {
+            $result = $item->getItemByName($itemName);
+            if ($result != NULL) {
+                break;
+            }
+        }
+        return $result;
     }
 }
 
--- Composite/client.php
+++ Composite/client.php
@@ -1,22 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Menu</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+</head>
+<body>
+MENU
+<hr>
+
 <?php
 include 'MenuComposite.php';
 
 $cake = new Item("蛋糕","70");
 $pudding = new Item("布丁","50");
-$dessert = new Menu(array($cake, $pudding));
+$dessert = new Menu(array($cake, $pudding), "甜點");
 
 $coffee = new Item("咖啡","80");
 $juice = new Item("果汁","70");
 $milktea = new Item("奶茶","50");
-
-$sideDish = new Menu(array($dessert, $coffee, $juice, $milktea));
-
+$sideDish = new Menu(array($coffee, $juice, $milktea, $dessert), "副餐");
 
 $cheeseBurger = new Item("起士漢堡","230");
 $fishburger = new Item("鱈魚堡","210");
-
-$mainCourst = new Menu(array($cheeseBurger, $fishburger));
-
-$menu = new Menu(array($sideDish, $mainCourst));
-
+$mainCourst = new Menu(array($cheeseBurger, $fishburger), "主餐");
+$menu = new Menu(array($sideDish, $mainCourst), "菜單");
 $menu->getNameAndPrice();
+
+$productResult = "";
+if (!empty($_GET["name"])) {
+    $reslut = $menu->getItemByName($_GET["name"]);
+    if ($reslut == NULL) {
+        $productResult = "not found.";
+    } else {
+        $productResult = $reslut;
+    }
+}
+?>
+
+<hr>
+    <form action="client.php">
+        <input type="text" name="name">
+        <input type="submit" value="search">
+    </form>
+    <br>
+    <div><?php echo $productResult;?></div>
+</body>
+</html>
